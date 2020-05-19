6da27a7aac3acd215a0715587c6255e3d890987d
Kotovayasya <kotovayasya@gmail.com>
2020-05-19 22:29:39 +0300

    Выводит Привет, Яна!

--- hello.php
+++ hello.php
@@ -1,5 +1,7 @@
-<?ph
+<?php
 
-var string = '0J/RgNC40LLQtdGCLCDQr9C90LAh';
+$string = '0J/RgNC40LLQtdGCLCDQr9C90LAh';
 
-echo base64decode($string) . PHP_EOL
+echo base64_decode($string) . PHP_EOL
+
+?>
